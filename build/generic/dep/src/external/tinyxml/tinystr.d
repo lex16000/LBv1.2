a65b6ff8f846057a35d7e73b5aa396cc7a1c593f
lex <schulz_a_alexander@hotmail.com>
2018-03-14 19:09:41 +0100

    initial commit

---
+++ build/generic/dep/src/external/tinyxml/tinystr.d
@@ -0,0 +1,8 @@
+build/generic/obj/src/external/tinyxml/tinystr.o: src/external/tinyxml/tinystr.cpp /usr/include/stdc-predef.h \
+ src/external/tinyxml/tinystr.h /usr/include/assert.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h /usr/include/string.h \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include/stddef.h /usr/include/xlocale.h \
+ /usr/include/x86_64-linux-gnu/bits/string3.h

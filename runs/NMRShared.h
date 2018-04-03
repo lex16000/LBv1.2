d2c2dad66699f944a382ca80415d8b7580c9c0fe
lex16000 <schulz_a_alexander@hotmail.com>
2018-04-03 08:21:20 +0200

    NMRshared for flux at distinct slices

---
+++ runs/NMRShared.h
@@ -0,0 +1,11 @@
+#ifndef NMRSHARED_H
+#define NMRSHARED_H
+
+#include <iostream>
+
+void test_function(std::vector<T> vec)
+{
+	std::cout << "called the test function: " << vec[0] << std::endl;
+}
+
+#endif
